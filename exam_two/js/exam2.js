73440ad114067231458f30466649ee1b5aaee0a1
onwudiwee <113317064+onwudiwee@users.noreply.github.com>
2026-05-25 07:49:34 -0400

    Refactor exam2.js with new question numbers and events
    Updated question numbers and added event listeners for h1 and figure elements.

--- exam_two/js/exam2.js
+++ exam_two/js/exam2.js
@@ -1,26 +1,71 @@
 
-// Part 3 Question 3
+// Part 3 Question 12
+const names = document.querySelectorAll(".addEventListeners");
+console.log("Nkwa", names.length, ".addEventListeners"); 
 
 
 
-
-// Part 3 Question 4
-
-
-
-
-
-// Part 3 Question 6
+// Part 3 Question 13
+document.querySelector("h1").addEventListener("click", function() {
+  console.log("Removing the h1")
+  this.style.display = "none"
+})
 
 
 
-
-
-// Part 3 Question 7
+// Part 3 Question 14
 
 
 
 
 
+// Part 3 Question 15
+document.querySelector("figure:nth-chi(1)").addEventListener("mouseover", function() {
+    console.log("Change first figure caption's text color")
+    this.style.visibility = "change"
+ })
 
-// Part 3 Question 8
+
+
+
+
+// Part 3 Question 16
+
+
+
+
+// Part 3 Question 17
+.lightboxOverlay {
+  position: fixed;
+  inset: 0;
+  z-index: 9999;
+  background-color: var(--lb-overlay-bg);
+  opacity: var(--lb-overlay-opacity);
+  display: none;
+}
+
+.lightbox {
+  position: fixed;
+  left: 0;
+  width: 100%;
+  z-index: 10000;
+  text-align: center;
+  line-height: 0;
+  font-weight: normal;
+  outline: none;
+}
+
+.lightbox .lb-image {
+  display: block;
+  height: auto;
+  max-width: inherit;
+  max-height: none;
+  border-radius: var(--lb-border-radius);
+
+  /* Image border */
+  border: var(--lb-image-border-width) solid var(--lb-image-border-color);
+}
+
+.lightbox a img {
+  border: none;
+}
